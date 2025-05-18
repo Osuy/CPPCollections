33118ece62a544d1119ae4df7811ee0ae0752a92
Osuy <1983373329@qq.com>
2025-05-18 18:46:25 +0800

    5/18

--- Effective C++/07_template/42：typename.h
+++ Effective C++/07_template/42：typename.h
@@ -1,10 +1,24 @@
 #pragma once
+namespace case42
+{
 /*
-	作用一：用于模板列表，和class没有区别
-
-	作用二：标识嵌套类型名称
-
-	不能用于继承父类生命和初始化列表
+	一、用于模板列表，和class没有区别
+	二、标识嵌套类型名称
+*/
+template<typename C>
+void func(C&& ctn)
+{
+	/*
+		value_type是C命名空间里的元素，但是C是模板参数
+		因此并不知道value_type到底是什么？如果是一个静态变量，那么下面的语句就是一个乘法表达式
+		如果是一个类型，那么就是定义了一个指针局部变量
+		为了在语法分析阶段让编译器知道这是类型，需要用typename指明
+		value_type 也称 模板参数从属类型
+	*/
+	C::value_type* p;
+}
+/*
+	typename不能用于继承父类生命和初始化列表
 	template<class T>
 	class ClassD : public ClassB<T>::InnerClass // 此处无需加 typename
 	{
@@ -18,3 +32,4 @@
 	虽然在具象化之后就能确定是类型，但是在具象化之前，编译期需要语法分析，它必须要求代码的正确性
 	而此时不能知道是什么。通过typename可以告知编译器这是一个类模板的一个内部类型，让编译期在语法分析阶段不会报错
 */
+}
