976fba047836d6914633d2ac5f8eaefccb8b9e36
Osuy <1983373329@qq.com>
2025-06-11 18:08:09 +0800

    6/10

---
+++ Memory Management jjhou/16.new_handler.h
@@ -0,0 +1,4 @@
+#pragma once
+/*
+	new/delete/new[]/delete[]可以 = delete ，不能 = default
+*/
---
+++ Memory Management jjhou/17.std_allocator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*
+	vc6、bc5、G2.9（现代版本未知）的std.alloctor并没有特殊设计，而是直接调用 ::operator new/delete
+
+	其中G2.9的容器并没有使用allocator，而是使用了alloc。它是经过设计的
+	在G4.9里，也同大伙一样用回了标准了allocator，而alloc改名为pool_alloctor
+
+*/
