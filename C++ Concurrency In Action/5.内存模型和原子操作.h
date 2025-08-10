667d8f59f81172cf9302850d83e536e8591cc886
osuy <osuy@DESKTOP-B7KVJLE>
2025-08-10 16:55:23 +0800

    8/9

---
+++ C++ Concurrency In Action/5.内存模型和原子操作.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+	5.3同步操作和强制次序
+
+	六种内存次序：
+		memory_order_relaxed
+		memory_order_consume
+		memory_order_acquire
+		memory_order_release
+		memory_order_acq_rel
+		memory_order_seq_cst（最严格）
+	 
+*/
